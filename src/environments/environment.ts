632cf01cf99943cb379a3293ac573ba5099bd41f
tuan anh <97lynk@gmail.com>
2019-12-25 13:14:13 +0700

    fix enviroment local host

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,7 @@
 export const environment = {
   production: false,
   redirectUri: 'http://localhost:8100',
-  // serviceUrl: 'http://192.168.1.10:8080',
-  serviceUrl: 'https://grabtho.tk',
+  serviceUrl: 'http://localhost:8080',
   authSsl: false,
   tag: 'dev',
   fbAppId: '1153417668380667',
